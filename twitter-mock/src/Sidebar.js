38d6dd97ef5035e21029f40a469a670a18389bf7
Jeremy King <jeremyking@Jeremys-MBP.fios-router.home>
2021-02-12 22:02:28 -0500

    Side bar complete

--- twitter-mock/src/Sidebar.js
+++ twitter-mock/src/Sidebar.js
@@ -1,13 +1,32 @@
 import React from "react";
 import './Sidebar.css'
-import TwitterIcom from '@material-ui/icons/twitter';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import SidebarOption from './SidebarOption';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import PermIdentityIcon from '@material-ui/icons/PermIdentity';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import {Button}from '@material-ui/core/'
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
            {/* Twitter Icon */}
-            <TwitterIcon />
+            <TwitterIcon className="twitterBird"/>
+            <SidebarOption active Icon={HomeIcon} text="Home"/>
+            <SidebarOption Icon={SearchIcon} text="Explore"/>
+            <SidebarOption Icon={NotificationsNoneIcon } text="Notifications"/>
+            <SidebarOption Icon={MailOutlineIcon} text="Messages"/>
+            <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks"/>
+            <SidebarOption Icon={ListAltIcon} text="Lists"/>
+            <SidebarOption Icon={PermIdentityIcon} text="Profile"/>
+            <SidebarOption Icon={MoreHorizIcon} text="More"/>
            {/* Button -> Twweet */}
+            <Button variant="outlined" className="sidebar-tweet" fullWidth>Tweet</Button>
         </div>
     );
 };
