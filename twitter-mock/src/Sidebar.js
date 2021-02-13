d87b7d1640e644ffccab8d1c419f38d13d5de329
Jeremy King <jeremyking@Jeremys-MBP.fios-router.home>
2021-02-12 20:32:33 -0500

    hello

---
+++ twitter-mock/src/Sidebar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Sidebar.css'
+import TwitterIcom from '@material-ui/icons/twitter';
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+           {/* Twitter Icon */}
+            <TwitterIcon />
+           {/* Button -> Twweet */}
+        </div>
+    );
+};
+
+export default Sidebar;
